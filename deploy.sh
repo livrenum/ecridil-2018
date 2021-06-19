b7d6cd31b5add7943f7699fe34c785bfd97291be
Louis-Olivier Brassard <louis@loupbrun.ca>
2021-06-19 17:47:47 -0400

    update deploy.sh for livre-defi-design.arcanes.ca
    using surge.sh for deploy/hosting

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-rsync -vrz --exclude-from=exclude.deploy --delete site/ louis@demohybrilivre.loupbrun.ca:/var/www/demohybrilivre.loupbrun.ca/
+#rsync -vrz --exclude-from=exclude.deploy --delete site/ louis@demohybrilivre.loupbrun.ca:/var/www/demohybrilivre.loupbrun.ca/
+
+# Surge.sh
+surge ./site --domain livre-defi-design.arcanes.ca
